36cd68b67eec295e0bb154386fb8305ccfde9f5b
Stanislav Silin <byme.work@gmail.com>
2016-12-14 16:02:18 +0200

    Implement level generation

---
+++ Assets/Code/GameController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LevelCreator))]
+public class GameController : MonoBehaviour
+{
+    public GameObject LevelRoot;
+
+	void Start ()
+    {
+        var levelCreator = this.GetComponent<LevelCreator>();
+        levelCreator.CreateLevel(this.LevelRoot, 10);
+	}
+}
---
+++ Assets/Code/LevelCreator.cs
@@ -0,0 +1,140 @@
+﻿using Assets.Code.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class LevelCreator : MonoBehaviour
+{
+    private System.Random Random = new System.Random();
+
+    public GameObject Ground;
+    public GameObject Wall;
+
+#if DEBUG
+    public GameObject Debug;
+#endif
+
+    public void CreateLevel(GameObject root, int size)
+    {
+        var worldSceme = this.CreateWorldSceme(size);
+
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+            {
+                GameObject cellTemplate = null;
+                if (worldSceme[i][j] == 1)
+                    cellTemplate = this.Ground;
+                else
+                    cellTemplate = this.Wall;
+
+                var cell = GameObject.Instantiate<GameObject>(cellTemplate, new Vector3(i, j, 0), Quaternion.identity);
+                cell.transform.parent = root.transform;
+            }
+    }
+
+    private int[][] CreateWorldSceme(int size)
+    {
+        var basePoints = this.GenerateBasePoints(size).Distinct().ToArray();
+
+#if DEBUG
+        foreach (var point in basePoints)
+            GameObject.Instantiate(this.Debug, new Vector3(point.Coords.x, point.Coords.y, -1), Quaternion.identity);
+#endif
+        var linksBetweenPoints = this.GenerateLinks(basePoints).ToArray();
+
+        var worldSceme = new int[size][];
+        for (int i = 0; i < size; i++)
+            worldSceme[i] = new int[size];
+
+        foreach (var point in basePoints)
+            worldSceme[(int)point.Coords.x][(int)point.Coords.y] = 1;
+
+        this.DrawWays(worldSceme, basePoints, linksBetweenPoints);
+
+        return worldSceme;
+    }
+
+    private void DrawWays(int[][] worldSceme, Point[] basePoints, Edge[] linksBetweenPoints)
+    {
+        foreach (var link in linksBetweenPoints)
+        {
+            var firstPoint = basePoints[link.FirstIndex];
+            var secondPoint = basePoints[link.SecondIndex];
+            var currentCoords = firstPoint.Coords;
+
+            while (currentCoords != secondPoint.Coords)
+            {
+                bool changed = false;
+                if (currentCoords.x != secondPoint.Coords.x)
+                {
+                    if (currentCoords.x > secondPoint.Coords.x)
+                        currentCoords.x--;
+                    else
+                        currentCoords.x++;
+                    changed = true;
+                }
+
+                if (currentCoords.y != secondPoint.Coords.y && !changed)
+                {
+                    if (currentCoords.y > secondPoint.Coords.y)
+                        currentCoords.y--;
+                    else
+                        currentCoords.y++;
+                }
+
+                worldSceme[(int)currentCoords.x][(int)currentCoords.y] = 1;
+            }
+        }
+    }
+
+    private IEnumerable<Edge> GenerateLinks(Point[] points)
+    {
+        foreach (var point in points)
+        {
+            var futherPoint = points.
+                OrderBy(o => Vector3.Distance(o.Coords, point.Coords)).
+                //Reverse().
+                Take(3);
+
+            yield return new Edge
+            {
+                FirstIndex = point.Index,
+                SecondIndex = futherPoint.First().Index
+            };
+
+            foreach (var closePoint in futherPoint.Skip(1))
+            {
+                //if (this.Random.NextDouble() > 0.7)
+                    yield return new Edge
+                    {
+                        FirstIndex = point.Index,
+                        SecondIndex = closePoint.Index
+                    };
+            }
+        }
+    }
+
+    private IEnumerable<Point> GenerateBasePoints(int size)
+    {
+        var count = size;
+        for (int i = 0; i < count; i++)
+            yield return new Point
+            {
+                Index = i,
+                Coords = new Vector2(this.Random.Next(0, size), this.Random.Next(0, size))
+            };
+    }
+
+    class Point
+    {
+        public Vector2 Coords;
+        public int Index;
+    }
+
+    class Edge
+    {
+        public int FirstIndex;
+        public int SecondIndex;
+    }
+}

33715159d26afd03c81eadb5f74d06a028dd2d4e
Stanislav Silin <byme.work@gmail.com>
2016-12-15 23:36:06 +0200

    Add records seralization support

---
+++ Assets/Code/Seralization/User.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Assets.Code.Seralization
+{
+    public enum GameEndReason
+    {
+        MummyDeath,
+        ZombieDeath,
+        Quit
+    }
+
+    public class User
+    {
+        [XmlAttribute("Name")]
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        [XmlAttribute("Coins")]
+        public int Coins
+        {
+            get;
+            set;
+        }
+
+        [XmlAttribute("Time")]
+        public float Time
+        {
+            get;
+            set;
+        }
+
+        [XmlAttribute("LaunchTime")]
+        public DateTime LaunchTime
+        {
+            get;
+            set;
+        }
+
+        [XmlAttribute("Reason")]
+        public GameEndReason Reason
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Assets/Code/Seralization/UserRespository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Assets.Code.Seralization
+{
+    public class UserRepository : IDisposable
+    {
+        private const string FileName = "Records.txt";
+
+        public UserRepository()
+        {
+            var serializer = new XmlSerializer(typeof(User[]));
+            using (var stream = new FileStream(FileName, FileMode.OpenOrCreate))
+            {
+                if (stream.Length == 0)
+                {
+                    serializer.Serialize(stream, new User[0]);
+                    stream.Seek(0, SeekOrigin.Begin);
+                }
+                
+                var users = serializer.Deserialize(stream) as User[];
+                this.Users = users.ToList();
+            }
+        }
+
+        public List<User> Users
+        {
+            get;
+            set;
+        }
+
+        public void Dispose()
+        {
+            var serializer = new XmlSerializer(typeof(User[]));
+            using (var stream = new FileStream(FileName, FileMode.Create))
+            {
+                serializer.Serialize(stream, this.Users.ToArray());
+            }
+        }
+    }
+}
